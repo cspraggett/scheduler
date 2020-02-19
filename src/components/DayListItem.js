e01a86fffd61e9370658456f29b67224c169a747
cspraggett <cspraggett@gmail.com>
2020-02-18 19:52:36 -0500

    start DayList

---
+++ src/components/DayListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import classnames from "classnames";
+
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const formatSpots = ((spots) => !spots ? "no spots remaining" :
+  spots === 1 ? "1 spot remaining" :
+    `${spots} spots remaining`)
+  
+  const dayClass = classnames({
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+  
+  return (
+    <li 
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
