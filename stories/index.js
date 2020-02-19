8b9e0c3f642fd7b97e6712a5ecfab0a0a7ab6d9e
cspraggett <cspraggett@gmail.com>
2020-02-18 21:38:27 -0500

    Add Form component to appointment

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -122,7 +123,7 @@ storiesOf("Button", module)
         .add("Initial", () => (
           <InterviewerList
             interviewers={interviewers}
-            setInterviewer={action("setInterviewer")}
+            onChange={action("setInterviewer")}
           />
         ))
         .add("Preselected", () => (
@@ -151,3 +152,11 @@ storiesOf("Button", module)
         .add("Status", () => <Status message="Deleting" />)
         .add("Error", () => <Error message="Could not delete appointment."
               onClose={action("onClose")} />)
+        .add("Form-Edit", () => <Form name="John Brown"
+              interviewers={interviewers}
+              interviewer={3}
+              onSave={action("onSave")}
+              onCancel={action("onCancel")} />) 
+        .add("Form-Create", () => <Form interviewers={interviewers}
+              onSave={action("onSave")}
+              onCancel={action("onCancel")} />)
