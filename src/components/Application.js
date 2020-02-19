40ad6cacce170241a382b0253c4799a78e54db8e
cspraggett <cspraggett@gmail.com>
2020-02-18 21:50:11 -0500

    added appointments to application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,65 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 import "components/Application.scss";
-
+import "components/DayList";
 import DayList from "components/DayList";
+// import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment";
+
+const appointments = [
+  {
+    id: 1,
+    time: "11am",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "John Brown",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png" ,
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Alice Walker",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png" ,
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Heather Bowles",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  }
+];
 
 const days = [
   {
@@ -23,8 +80,8 @@ const days = [
 ];
 
 export default function Application(props) {
-  const [day,setDay] = useState("Monday");
-
+  const [day, setDay] = useState("Monday");
+ 
   return (
     <main className="layout">
       <section className="sidebar">
@@ -34,12 +91,11 @@ export default function Application(props) {
   alt="Interview Scheduler"
 />
 <hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-  <DayList
+<nav className="sidebar__menu">{<DayList
   days={days}
   day={day}
   setDay={setDay}
-/></nav>
+/>}</nav>
 <img
   className="sidebar__lhl sidebar--centered"
   src="images/lhl.png"
@@ -47,7 +103,8 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => <Appointment key={appointment.id} {...appointment} />) }
+        {<Appointment key="last" time="5pm" />}
       </section>
     </main>
   );
