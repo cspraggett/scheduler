6a3072637aab21fef05bd393235eb6951a3aa071
cspraggett <cspraggett@gmail.com>
2020-02-26 13:41:11 -0500

    comment and lint useApplicationData.js

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,103 +1,53 @@
+// UseApplicationData takes in requests from Application and process any requests to the reducer
+// I could not figure out how to move async logic directly into the reducer so I have made
+// any axio calls in the functions here.
+
 import {useReducer, useEffect} from 'react';
 import axios from "axios";
 
 import reducer from "../reducers/application";
 import {SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW} from "../reducers/application";
 
-// export default function useApplicationData() {
-  // const SET_DAY = "SET_DAY";
-  // const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-  // const SET_INTERVIEW = "SET_INTERVIEW";
-  
-
-  // function reducer(state, action) {
-  //   switch (action.type) {
-  //     case SET_DAY:
-  //       return {...state, day:action.day}
-  //     case SET_APPLICATION_DATA:
-  //       return {...state, day:state.day, days:action.days, appointments:action.appointments, interviewers:action.interviewers}
-  //     case SET_INTERVIEW:
-  //       return {...action.newState,};
-    
-  //   default:
-  //     throw new Error(
-  //       `Tried to reduce with unsupported action type: ${action.type}`
-  //     );
-  //   }
-  // }
- 
-  export default function useApplicationData() {
-    const [state, dispatch] = useReducer(reducer, {day: "Monday", days: [], appointments: {}, interviewers: {}});
+export default function useApplicationData() {
+  const [state, dispatch] = useReducer(reducer, {day: "Monday", days: [], appointments: {}, interviewers: {}});
     
     
-    const setDay = day => dispatch({type: SET_DAY, day})//useReducer(reducer, {...state, day });
+  const setDay = day => dispatch({type: SET_DAY, day});
 
-
+  // This useEffect is triggered when the app loads and populates the state with data from the api server.
   useEffect(() => {
-    // webSocket.onopen = ((event) => {
-    //   webSocket.send('Ping');
-
-    // })
-    //     webSocket.onmessage = (event => {
-    //       console.log('message recieved:')
-    //     })
     Promise.all([
       axios.get("/api/days"),
       axios.get("/api/appointments"),
       axios.get("/api/interviewers")
     ])
-    .then((all) => {
-      dispatch({type: SET_APPLICATION_DATA, day: "Monday", days: all[0].data, appointments:
-        all[1].data, interviewers: all[2].data })
-    }) 
-  },[])
-
-  // useEffect(() => {
-  //   const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
-  //   // webSocket.onmessage = (event => {
-  //   //   console.log('message recieved:')
-  //   // }) 
-  //   webSocket.onopen = ((event) => {
-  //     // webSocket.send('ping');
-      
-  //   })
-  //   webSocket.onmessage = (event => {
-  //     const newData = JSON.parse(event.data);
-  //     console.log(newData);
-  //     // if (newData.type = SET_INTERVIEW && newData.id !== state.appointments.id) {
-  //     //   newData.interview ? bookInterview(newData.id, newData.interview) : cancelInterview(newData.id);
-  //     //   webSocket.send(SET_INTERVIEW)
-  //     // }
-  //     // console.log('socket ready state', webSocket.readyState)
-  //   })
-    
-        
-  // })
-
+      .then((all) => {
+        dispatch({type: SET_APPLICATION_DATA, day: "Monday", days: all[0].data, appointments:
+        all[1].data, interviewers: all[2].data });
+      });
+  },[]);
   
-
-  
-  // const appointments = getAppointmentsForDay(state, state.day);
-
+  // cancelInterview sends a DELETE request through axio to the API server and removes the interview
+  // from the database and then dispatches the new state to the reducer.
   const cancelInterview = (id) => {
     return axios.delete(`/api/appointments/${id}`)
-      .then(result => {
+      .then(() => {
         const newAppointments = state.appointments[id].interview = null;
         const newState = {...state, newAppointments};
 
+        //? for some reason this filter updates the count in the state, I don't
+        //? know if this is bad practice or not?
         newState.days.filter(curr => {
-        
+
           if (curr.name === newState.day) {
             curr.spots++;
           }
-          // return undefined;
-        }) 
-        dispatch({type: SET_INTERVIEW, newState: newState})
-      })
-      //.catch(err => console.log(err));
-  }
+        });
+        dispatch({type: SET_INTERVIEW, newState: newState});
+      });
+  };
 
-  function bookInterview(id, interview) {
+  const bookInterview = ((id, interview) => {
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
@@ -107,74 +57,52 @@ import {SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW} from "../reducers/applicat
       ...state.appointments,
       [id]: appointment
     };
-    // const newState = {...state, appointments}
-    // dispatch({ type: SET_INTERVIEW, newState: newState })
     return axios.put(`/api/appointments/${id}`, appointment)
-    .then(() => {
-      let newState = {};
-      console.log('appointment',appointment)
-      console.log('state', state.appointments[id].interview)
-      if (!state.appointments[id].interview || appointment !== state.appointments[id].interview){
-        newState = {...state, appointments}
-        if (!state.appointments[id].interview) {
-          newState.days.filter((curr) => {
-            if (curr.name === newState.day){
-                    curr.spots--;
-                  }
-                  return undefined
-          })
+      .then(() => {
+        let newState = {};
+        console.log('appointment',appointment);
+        console.log('state', state.appointments[id].interview);
+        if (!state.appointments[id].interview || appointment !== state.appointments[id].interview) {
+          newState = {...state, appointments};
+          if (!state.appointments[id].interview) {
+            //? Again I'm not sure if this is a good way to update spots in the state?
+            newState.days.filter((curr) => {
+              if (curr.name === newState.day) {
+                curr.spots--;
+              }
+              return undefined;
+            });
 
-          // dispatch({type: SET_APPLICATION_DATA, day: newState.day, days: newState.days, appointments: newState.appointments, interviewers: newState.interviewers})
+          }
+        } else {
+          newState = {...state};
         }
-      } else {
-        newState = {...state}
-      }
-      // console.log('oldState', state)
-      // console.log('newState', newState)
-      
-      //   console.log('curr', newState.appointments[id].interview)
-      //   console.log('old', state.days[id].appointments[3])
-        // if (newState.appointments[id].interview !== state.appointments[id].interview) {
-        //   newState.days.spots--;
-        // }
-        // newState = {...newState, newState.days.spots - 1}
-        // console.log('newState', newState.days.spots)
-      // newState.days.filter((curr, i) => {
-      //   // if (curr.name === newState.day) {
-      //   //   if (curr.name === state.day[curr.day].id[i]){
-      //   //     curr.spots--;
-      //   //   }
-      //   // }
-      //   return undefined;
-      // }) 
-      dispatch({ type: SET_INTERVIEW, newState: newState })
-    })
-    //.catch(err => console.log(err))//new PromiseRejectionEvent())
-    // dispatch({type: SET_INTERVIEW, id:id, interview:interview});
-  }
+        dispatch({ type: SET_INTERVIEW, newState: newState });
+      });
+  });
 
-    // return (
-    //   // dispatch({ type: SET_DAY, day }) ||
-    //   // dispatch({ type: SET_APPLICATION_DATA, days, appointments, interviewers }) ||
-    //   // dispatch({ type: SET_INTERVIEW, id, interview }) ||
-    //   // dispatch({ type: SET_INTERVIEW, id, interview: null }) 
-    // )
-    return {state, setDay, bookInterview, cancelInterview}
-  }
-  // const schedule = appointments.map((appointment) => {
-  //   const interview = getInterview(state, appointment.interview);
-  //   const interviewers = getInterviewersForDay(state, {day: state.day, interviewers:state.interviewers}) || [];
-  //   return (
-  //     <Appointment
-  //       key={appointment.id}
-  //       id={appointment.id}
-  //       time={appointment.time}
-  //       interview={interview}
-  //       interviewers={interviewers}
-  //       bookInterview={bookInterview}
-  //       cancelInterview={cancelInterview}
-  //       />
-  //   );
-  //   });
-  // return {state, setDay, bookInterview, cancelInterview}
-// }
+  return {state, setDay, bookInterview, cancelInterview};
+}
+
+//TODO Begining of webSocket implementaion.
+// useEffect(() => {
+//   const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+//   // webSocket.onmessage = (event => {
+//   //   console.log('message recieved:')
+//   // })
+//   webSocket.onopen = ((event) => {
+//     // webSocket.send('ping');
+      
+//   })
+//   webSocket.onmessage = (event => {
+//     const newData = JSON.parse(event.data);
+//     console.log(newData);
+//     // if (newData.type = SET_INTERVIEW && newData.id !== state.appointments.id) {
+//     //   newData.interview ? bookInterview(newData.id, newData.interview) : cancelInterview(newData.id);
+//     //   webSocket.send(SET_INTERVIEW)
+//     // }
+//     // console.log('socket ready state', webSocket.readyState)
+//   })
+    
+        
+// })
